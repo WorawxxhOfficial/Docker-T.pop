d3157b19b9865c3e439d8fb6d0732d650d2e6edc
A04987 <gasidit0anu@gmail.com>
2025-10-13 15:48:08 +0700

    Update login.admin.test.js

--- src/tests/login.admin.test.js
+++ src/tests/login.admin.test.js
@@ -1,2 +1,18 @@
 const { login } = require('../login');
 
+describe('Admin Login Test', () => {
+  test('should return true when admin uses correct password', () => {
+    const result = login('admin', '1234');
+    expect(result).toBe(true);
+  });
+
+  test('should return false when admin uses wrong password', () => {
+    const result = login('admin', 'wrongpass');
+    expect(result).toBe(false);
+  });
+
+  test('should return false when admin username is incorrect', () => {
+    const result = login('Admin', '1234'); // ตัวใหญ่ไม่ตรง
+    expect(result).toBe(false);
+  });
+});
