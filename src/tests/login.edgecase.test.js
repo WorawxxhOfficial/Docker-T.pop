69a4b23267d4897b48af8f4cfd7a88069b1ca206
Zoeytothemoon <worawichyimsabai@gmail.com>
2025-10-15 12:46:09 +0700

    add unit test edgecase

--- src/tests/login.edgecase.test.js
+++ src/tests/login.edgecase.test.js
@@ -1,2 +1,9 @@
 const { login } = require('../login');
 
+test('empty username/password fails', () => {
+  expect(login('', '')).toBe(false);
+});
+
+test('null username/password fails', () => {
+  expect(login(null, null)).toBe(false);
+});
